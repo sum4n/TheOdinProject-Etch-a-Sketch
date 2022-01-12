a4c5985bb526d8b1be6a9aad1037ad4de3947b66
Suman Das <sum4ndas@gmail.com>
2022-01-12 19:29:50 +0530

    Hover effect to change divs' color

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const body = document.querySelector('body');
 const parentDiv = document.createElement('div');
 parentDiv.classList.add("parentDiv");
 
+let idCounter = 0;
+
 for (let i = 0; i < 16; i++) {
     const childDiv = addChildDiv();
     for (let j = 0; j < 16; j++) {
-        addGrandChildDiv(childDiv, j);
+        idCounter += 1;
+        addGrandChildDiv(childDiv, j, idCounter);
         // grandChildDiv.textContent = j;
     }
 }
@@ -15,15 +18,26 @@ body.appendChild(parentDiv);
 
 function addChildDiv() {
     const childDiv = document.createElement('div');
-    childDiv.classList.add("childDiv")
+    childDiv.classList.add("childDiv");
     // childDiv.textContent = i;
     parentDiv.appendChild(childDiv);
     return childDiv;
 }
 
-function addGrandChildDiv(childDiv, j) {
+function addGrandChildDiv(childDiv, j, idCounter) {
     const grandChildDiv = document.createElement('div');
     grandChildDiv.classList.add("grandChildDiv");
-    grandChildDiv.textContent = j + 1;
+    grandChildDiv.textContent = idCounter;
+    // created div gets id.
+    grandChildDiv.id = idCounter;
     childDiv.appendChild(grandChildDiv);
 }
+
+
+const grandChildDivs = document.querySelectorAll('.grandChildDiv');
+
+grandChildDivs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.classList.add("blackBg");
+    });
+});
